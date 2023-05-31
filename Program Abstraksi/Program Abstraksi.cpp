62a8a8b7697b26112cd3ba4f7a922cc5888ad097
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-31 22:04:19 +0700

    menyembunyikan fungsi dan memakia class hak akses private dan public dan memakai metod void

---
+++ Program Abstraksi/Program Abstraksi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Abstraksiklas {
+private: string x, y;
+
+public :
+    //method untuk mengisi nilai 
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+// menampilkan nilai
+void display() {
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    }
+};
+int main() 
+{
+    std::cout << "Hello World!\n";
+}
+
+
