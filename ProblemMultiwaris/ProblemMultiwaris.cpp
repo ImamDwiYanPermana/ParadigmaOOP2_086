e6f7a8211a3311c0570dda57a8bb08a9a7672574
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-31 11:18:04 +0700

    membuat turunan public pekerja dan pelajar menjadi class bud dan membuat kosntruktor budi

--- ProblemMultiwaris/ProblemMultiwaris.cpp
+++ ProblemMultiwaris/ProblemMultiwaris.cpp
@@ -28,9 +28,19 @@ public:
         cout << " pelajar di buat dibuat " << umur << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << " budi dibuat \n" << endl;
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
 
