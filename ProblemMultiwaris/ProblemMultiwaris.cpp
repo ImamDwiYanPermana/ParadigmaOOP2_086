6600d47ec39c7d031ec37f0debf75dd7a3efe6d4
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-31 11:04:53 +0700

    membuat class orang dengan public

--- ProblemMultiwaris/ProblemMultiwaris.cpp
+++ ProblemMultiwaris/ProblemMultiwaris.cpp
@@ -1,6 +1,15 @@
 
 #include <iostream>
+using namespace std;
 
+class orang {
+public:
+    int umur;
+        orang(int pUmur):
+        umur(pUmur){
+            cout << "seorang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
